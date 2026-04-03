1183c73ec0117b15e3e6aee5842fab65aa4d0ac8
phornvaryr-star <phornvary.r@gmail.com>
2026-04-03 16:40:52 +0700

    Update resourceClass.php

--- app/Http/Resources/resourceClass.php
+++ app/Http/Resources/resourceClass.php
@@ -4,13 +4,9 @@ namespace App\Http\Resources;
 use App\Models\Category;
 use App\Models\User;
 use App\Models\Role;
-use App\Models\Product;
-use App\Http\Resources\CategoryResource;
-use App\Http\Resources\ProductResource;
 use App\Http\Resources\RoleResource;
 use App\Http\Resources\UserResource;
-use App\Models\Customer;
-use App\Http\Resources\CustomerResource;
+
 
 class ResourceClass
 {
