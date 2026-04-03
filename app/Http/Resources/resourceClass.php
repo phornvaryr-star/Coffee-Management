a4c3219bf772a91412e680804d355460f8c0aa4a
phornvaryr-star <phornvary.r@gmail.com>
2026-04-03 16:49:53 +0700

    Update resourceClass.php

--- app/Http/Resources/resourceClass.php
+++ app/Http/Resources/resourceClass.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Resources;
 use App\Models\User;
-use App\Models\Role;
-use App\Http\Resources\RoleResource;
 use App\Http\Resources\UserResource;
 class ResourceClass
 {
@@ -12,7 +10,6 @@ class ResourceClass
         $map = [
             User::class => UserResource::class,
             // Add more models here
-            Role::class => RoleResource::class,  
         ];
 
         return $map[get_class($model)] ?? null;
