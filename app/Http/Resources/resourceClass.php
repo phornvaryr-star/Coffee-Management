4d2200afe5847645411c9797831a61d889659625
phornvaryr-star <phornvary.r@gmail.com>
2026-04-03 16:43:30 +0700

    Update resourceClass.php

--- app/Http/Resources/resourceClass.php
+++ app/Http/Resources/resourceClass.php
@@ -1,25 +1,10 @@
 <?php
 
 namespace App\Http\Resources;
-<<<<<<< HEAD
-use App\Models\Category;
-use App\Models\User;
-use App\Models\Role;
-use App\Models\Product;
-use App\Http\Resources\CategoryResource;
-use App\Http\Resources\ProductResource;
-use App\Http\Resources\RoleResource;
-use App\Http\Resources\UserResource;
-use App\Models\Customer;
-use App\Http\Resources\CustomerResource;
-=======
 use App\Models\User;
 use App\Models\Role;
 use App\Http\Resources\RoleResource;
 use App\Http\Resources\UserResource;
-
->>>>>>> 281d74e (fix: User & Role Management)
-
 class ResourceClass
 {
     public static function resolve($model)
@@ -27,13 +12,6 @@ class ResourceClass
         $map = [
             User::class => UserResource::class,
             // Add more models here
-<<<<<<< HEAD
-            Product::class => ProductResource::class,
-            Category::class => CategoryResource::class,
-            Role::class => RoleResource::class,
-            Customer::class => CustomerResource::class,
-            
-=======
             Role::class => RoleResource::class,  
 >>>>>>> 281d74e (fix: User & Role Management)
         ];
