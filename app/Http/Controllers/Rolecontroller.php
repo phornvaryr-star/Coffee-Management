9539f15a32428b8c27805234cb3b9296c9e538d5
phornvaryr-star <phornvary.r@gmail.com>
2026-04-03 16:17:51 +0700

    Update Rolecontroller.php

--- app/Http/Controllers/Rolecontroller.php
+++ app/Http/Controllers/Rolecontroller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Role;
 use App\Http\Resources\RoleResource;
+use App\Http\Resources\BaseCollection;
 use Illuminate\Http\Request;
 
 class Rolecontroller extends Controller
@@ -10,14 +11,14 @@ class Rolecontroller extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $roles = Role::all();
-        return response()->json([
-            'status' => true,
-            'message' => 'Role list',
-            'data' => RoleResource::collection($roles),
-        ]);
+        $query = Role::query();
+        $sortBy = $request->get('sort_by', 'id');
+        $sortOrder = $request->get('sort_order', 'desc');
+        $query->orderBy($sortBy, $sortOrder);
+        $roles = $query->get();
+        return (new BaseCollection($roles, RoleResource::class))->setMessage("Roles retrieved successfully");
     }
 
     /**
